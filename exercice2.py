ecebb7e5566c376aef8d73f5fda1fb458d0c4922
SofKh <sofiane.chkh@gmail.com>
2022-03-04 15:10:40 -0500

    definition des fonctions

---
+++ exercice2.py
@@ -0,0 +1,27 @@
+
+def add_fract(fr1, fr2) :
+    a, b = fr1
+    d, c = fr2
+    return a * c + b * d, b * c
+
+def sous_fract(fr1, fr2) :
+    a, b = fr1
+    d, c = fr2
+    return a * c - d * b, b * c
+
+def mult_fract(fr1, fr2) :
+    a, b = fr1
+    d, c = fr2
+    return a * d, b * c
+
+def div_fract(fr1, fr2) :
+    a, b = fr1
+    d, c = fr2
+    return a * c / b * d
+
+ 
+
+
+
+
+
